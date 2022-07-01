cdad585d5d1b17c74afe80f999c246b01cc3bbed
Rasmus Rendal <rasmus@rend.al>
2022-07-01 08:52:12 +0200

    Update the flake, add github actions

--- flake.nix
+++ flake.nix
@@ -1,33 +1,50 @@
 {
   description = "UPPAAL DBM Library";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-21.11";
+  inputs.nixpkgs.url = "nixpkgs/master";
+  inputs.uutils.url = "github:UPPAALModelChecker/UUtils";
 
-  outputs = { self, nixpkgs }:
-  let
+  outputs = { self, nixpkgs, uutils }:
+    let
       # System types to support.
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       # Nixpkgs instantiated for supported system types.
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-  in
-  {
-    packages = forAllSystems (system:
-    let
-      pkgs = nixpkgsFor.${system};
+      crossNixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        crossSystem = nixpkgs.lib.systems.examples.mingwW64;
+      });
+
     in
-    {
-      UDBM = pkgs.stdenv.mkDerivation {
-        pname = "UDBM";
-        version = "0.0.0";
-        src = ./.;
-      };
-    });
-    devShell = forAllSystems (system:
-    let pkgs = nixpkgsFor.${system};
-    in pkgs.mkShell {
-      buildInputs = with pkgs; [ cmake doctest boost ninja clang-tools ];
-    });
-  };
+    rec {
+      library = (pkgs: nativePkgs:
+        uutils.addCrossCheck pkgs nativePkgs (pkgs.stdenv.mkDerivation {
+          pname = "UDBM";
+          version = "1.2.0";
+          src = ./.;
+          nativeBuildInputs = with nativePkgs; [ cmake ];
+          propagatedBuildInputs = [ (uutils.library pkgs nativePkgs) ];
+          buildInputs = with pkgs; [ doctest boost174 ];
+          cmakeFlags = [ "-DTESTING=ON" ];
+          doCheck = true;
+        }));
+
+
+      packages.udbm = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        self.library pkgs pkgs);
+
+      packages.crossPackage = forAllSystems (system:
+        let
+          nativePkgs = nixpkgsFor.${system};
+          pkgs = crossNixpkgsFor.${system};
+        in
+        self.library pkgs nativePkgs);
+
+      defaultPackage = packages.udbm;
+    };
 }
